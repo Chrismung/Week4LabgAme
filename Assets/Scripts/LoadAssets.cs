17cd8d31585367f8e97c13db4f0aa92c180e4974
Chrismung <142159288+Chrismung@users.noreply.github.com>
2024-09-01 02:20:01 +1000

    60 percent complete

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class LoadAssets : MonoBehaviour
 {
+
+    public GameObject redObj;
     // Start is called before the first frame update
-    GameObject redObj;
     void Start()
     {
         
